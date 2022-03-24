65544e46b77a92c82f3bd8884dc60aaeb7deb4cb
admin <sarteisaac09@gmail.com>
2022-03-24 13:12:35 +0800

    added feedback model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :feedbacks, dependent: :destroy
+
   validates :fullname, presence: true
   validates :email, presence: true
   validates :phone, presence: true
