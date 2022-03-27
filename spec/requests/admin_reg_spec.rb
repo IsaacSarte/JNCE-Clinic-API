0606f9b5e79eed46743a1c5d0f24c3b86c450b69
Isaac Sarte <aizakku09@gmail.com>
2022-03-27 18:09:03 +0800

    added admin auth requests spec

---
+++ spec/requests/admin_reg_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Registrations', type: :request do
+  describe 'POST /admins' do
+    it 'registers' do
+      post '/admins/', params: { admin: {
+        email: 'admin@email.com',
+         password: 'password'
+      } }
+
+      expect(response).to have_http_status(:created)
+      #   expect(response.headers['Authorization']).to include('Bearer')
+    end
+  end
+end
