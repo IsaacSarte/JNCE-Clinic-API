dbcb14118ea69012e937b092d1b33e2970249c66
Argie Barcena <barcenaargie@gmail.com>
2022-03-28 00:38:07 +0800

    Refactoring create method for creating user with feedback for front end responses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::API
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::ParameterMissing, with: :params_not_found
 
   private
 
   def record_not_found
     render json: { status: 404, data: { error: 'Record your trying to access was not found.' } }, status: :not_found
+  end
+
+  def params_not_found
+    render json: { status: 400, data: { error: 'Please kindly fill out all required fields.' } }, status: :bad_request
   end
 end
