f139c51c1e734476424925d3cf124c5639b597f8
Isaac Sarte <aizakku09@gmail.com>
2022-04-02 12:42:15 +0800

    changed phone datatype to get 0 in front

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
 
   validates :fullname, presence: true
   validates :email, presence: true, format: { with: Devise.email_regexp }
-  validates :phone, presence: true
+  validates :phone, presence: true, length: { minimum: 11, maximum: 11 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_140613) do
+ActiveRecord::Schema.define(version: 2022_04_02_043552) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_140613) do
   create_table "users", force: :cascade do |t|
     t.string "fullname"
     t.string "email"
-    t.bigint "phone"
+    t.string "phone"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
