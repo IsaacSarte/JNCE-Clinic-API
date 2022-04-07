5cb96cc2f7c49c66e79573ea7bb1ea044be66e00
Argie Barcena <barcenaargie@gmail.com>
2022-04-07 14:47:04 +0800

    fix rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,9 @@ class ApplicationController < ActionController::API
   rescue_from ActionController::ParameterMissing, with: :params_not_found
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
+
   private
+
   def record_not_found
     render json: { status: 404, data: { error: 'Record your trying to access was not found.' } }, status: :not_found
   end
@@ -14,10 +15,10 @@ class ApplicationController < ActionController::API
     render json: { status: 400, data: { error: 'Please kindly fill out all required fields.' } }, status: :bad_request
   end
 
-
   protected
+
   def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password)}
-      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password)}
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
